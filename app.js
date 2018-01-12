ebe5fc98912461d5eb1bc88aca7f9a961cabd090
Emmanuel AKINDE <ibukunbamise@yahoo.com>
2018-01-12 18:35:09 +0100

    styling the log

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ new Vue({
             var damage = this.calculateDamage(10,20);
             this.monsterHealth -= damage;
             this.turns.unshift({
-                isPlayer: false,
+                isPlayer: true,
                 text: 'Player hits Monster hard for ' + damage
             })
             if(this.checkWin()){
@@ -53,7 +53,7 @@ new Vue({
                 this.playerHealth = 100;
             }
             this.turns.unshift({
-                isPlayer: false,
+                isPlayer: true,
                 text: 'Player heals for 10'
             });
             this.monsterAttacks();
