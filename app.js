23c9eb2b09fd5f9824d43d4ccb2a3a8d663d408e
Emmanuel AKINDE <ibukunbamise@yahoo.com>
2018-01-12 17:08:05 +0100

    implementing special attack

--- app.js
+++ app.js
@@ -16,10 +16,18 @@ new Vue({
             if(this.checkWin()){
                 return;
             }
-            this.playerHealth -= this.calculateDamage(12,5);
+            this.monsterAttacks();
         },
         specialAttack: function () {
-
+            this.monsterHealth -= this.calculateDamage(10,20);
+            if(this.checkWin()){
+                return;
+            }
+            this.monsterAttacks();
+        },
+        monsterAttacks: function () {
+            this.playerHealth -= this.calculateDamage(5,12);
+            this.checkWin()
         },
         heal : function () {
 
