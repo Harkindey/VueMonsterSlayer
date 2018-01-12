8a2d9c9976770a601fb46a43ae9346d07bff8ba6
Emmanuel AKINDE <ibukunbamise@yahoo.com>
2018-01-12 16:54:05 +0100

    Refactoring

--- app.js
+++ app.js
@@ -12,23 +12,11 @@ new Vue({
             this.monsterHealth = 100;
         },
         attack: function () {
-            var max = 10;
-            var min = 3;
-            var damage = Math.max(Math.floor(Math.random() * max) + 1, min);
-            this.monsterHealth -= damage;
-            if( this.monsterHealth <= 0){
-                alert('You Won');
-                this.gameIsRunning = false;
-                return
+            this.monsterHealth -= this.calculateDamage(3,10);
+            if(this.checkWin()){
+                return;
             }
-            max = 12;
-            min = 5;
-            damage = Math.max(Math.floor(Math.random() * max) + 1, min);
-            this.playerHealth -= damage;
-            if( this.playerHealth <= 0){
-                alert('You Lost');
-                this.gameIsRunning = false;
-            }
+            this.playerHealth -= this.calculateDamage(12,5);
         },
         specialAttack: function () {
 
@@ -38,6 +26,27 @@ new Vue({
         },
         giveUp : function () {
 
+        },
+        calculateDamage: function (min, max){
+            return Math.max(Math.floor(Math.random() * max) + 1, min);
+        },
+        checkWin: function () {
+            if( this.monsterHealth <= 0){
+                if(confirm('You Won! New Game?')){
+                    this.startGame();
+                } else {
+                    this.gameIsRunning = false;
+                }
+                return true;
+            } else if (this.playerHealth <= 0) {
+                if(confirm('You Lost! New Game?')){
+                    this.startGame();
+                } else {
+                    this.gameIsRunning = false;
+                }
+                return true;
+            }
+            return false;
         }
     }
 })
